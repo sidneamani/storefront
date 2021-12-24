97aaea9f07b33926c000800c12385c286da03a71
sidneamani <64225544+sidneamani@users.noreply.github.com>
2021-12-24 14:41:27 -0500

    Update settings.py

--- storefront/settings.py
+++ storefront/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'tags',
     'django.contrib.sites'
 ]
-SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
